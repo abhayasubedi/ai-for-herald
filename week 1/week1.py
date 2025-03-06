1956894f6b18ace8ee228d5dcc10821f7b9b0e9e
abhayasubedi <119160666+abhayasubedi@users.noreply.github.com>
2025-03-06 14:24:38 +0545

    week 1 and two

---
+++ week 1/week1.py
@@ -0,0 +1,12 @@
+from PIL import Image
+image = Image.open("C:\\Users\\Asus\\OneDrive\\Desktop\\herald\\AI\\Lenna_(test_image).png")
+#image.show()
+print("Image Size:",image.size)
+print("image format:",image.format)
+print("image mode:",image.mode)
+
+
+
+
+
+
